7c3f349da2f4c90e4646d8c0707ba124dc9bddf5
Svetlana <fidelity-67rus@mail.ru>
2022-07-28 09:47:15 +0300

    feat: add test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+  const mathN = Math.abs(n);
+  const arr = Array.from(mathN.toString());
+  const arrRev = arr.reverse();
+  const str = arrRev.join("");
+  return Number(str);
 }
